10318a10a473fc9904e6d2fd4a2fb854eefbd60b
d1611234 <d1611234@gmail.com>
2026-06-22 16:35:11 +0800

    網頁乙及第2題

---
+++ front/forgot.php
@@ -0,0 +1,24 @@
+<fieldset style="width:50%;margin:auto;">
+    <legend>
+        忘記密碼
+    </legend>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>請輸入信箱以查詢密碼</td>
+            </tr>
+            <tr>
+                <td><input type="text" name="email" id="email" style="width: 150%;"></td>
+            </tr>
+            <tr>
+                <td id="result"></td>
+            </tr>
+            <tr>
+                <td>
+                    <div>您的密碼:</div>
+                    <button>尋找</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+</fieldset>
---
+++ front/reg.php
@@ -0,0 +1,66 @@
+<fieldset style="width:60%;margin:auto;">
+    
+    <legend>
+        會員註冊
+        <div style="color: red;">*請設定您要註冊的帳好及密碼(最長12個字元)</div>
+    </legend>
+    <form action="" method="post">
+        <table>
+        <tr>
+        
+            <td>Step1:登入帳號</td>
+            <td><input type="text" name="acc" id="acc" style="width: 100%;"></td>
+        </tr>
+        <tr>
+            <td>Step2:登入密碼</td>
+            <td><input type="password" name="pw" id="pw" style="width: 100%;"></td>
+        </tr>
+        <tr>
+            <td>Step3:再次確認密碼</td>
+            <td><input type="password2" name="pw2" id="pw2" style="width: 100%;"></td>
+        </tr>
+        <tr>
+            <td>Step4:信箱(忘記密碼使用)</td>
+            <td><input type="text" name="email" id="email" style="width: 100%;"></td>
+        </tr>
+        <tr>
+            <td>
+                <button type="button" onclick="reg()">註冊</button>
+                <button type="rest" onclick="$('#acc,$pw,#email').val('')">清除</button>
+            </td>
+            <td></td> </tr>
+            <!-- <td>
+                <a href="?do=forgot">忘記密碼</a>
+                <a href="?do=reg">尚未註冊</a>
+            </td> -->
+        </tr>
+        </table>
+    </form>
+</fieldset>
+<script>
+    function reg(){
+        let user={
+            'acc':$("#acc").val(),
+            'pw':$("#pw").val(),
+            'pw2':$("#pw2").val(),
+            'email':$("#email").val(),
+        }
+        if(user.acc=="" || user.pw==""||user.pw2==""|| user.email==""){
+        alert("不可空白");
+        }else if (user.pw !=user.pw2){
+            alert("密碼錯誤");
+        }else {
+            $.get("api/chk_acc.php",user,(res)=>{
+                if(parseInt(res)>0){
+                    addEventListener("帳號重複");
+                }else{
+                    $.post("api/reg.php",user,()=>{
+                        alert("註冊成功，歡迎加入")
+                           location.href='?do=login'
+                    })
+                }
+            })
+
+        }
+    }
+</script>
